054a3f1603b26ebcc4e538a66d60030a17543d8c
sujaygijre <30726091+sujaygijre@users.noreply.github.com>
2017-08-06 20:19:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,52 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content={
+    title:'article one:sujay gijre',
+    heading:'article one',
+    date:'sep 5,2016',
+    content:` <p>
+                    this a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first aricle
+                    </p>
+                     <p>
+                    this a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first aricle
+                    </p>
+                     <p>
+                    this a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first ariclethis a text for my first aricle
+                    </p>`
+} ;
+function createfunction(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=content;
+}
+var htmltemplate=`<html>
+    <head>
+        <title>
+           ${title}
+            </title>
+            <meta name="viewport" content="width+device+width,initial-scale=1"/>
+             <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div>
+                <a href="/">HOME</a>
+                </div>
+                <hr/>
+                <h3>  ${heading}</h3>
+                <div>  ${date}</div>
+                <div>
+                   ${content}
+                    </div>
+            </body>
+            
+</html>`
+;
+return htmltemplate
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(articleone));
 });
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
